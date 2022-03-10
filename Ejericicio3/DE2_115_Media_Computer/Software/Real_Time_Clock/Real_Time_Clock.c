86f94a26f36f54fc4f39a8ab6bdccb4fd604562a
Jose Luis Rocabado Rocha <JOROROC@alumno.upv.es>
2022-03-10 20:29:55 +0100

    commit adeñantado con el clock, los minutos no se representan bien

---
+++ Ejericicio3/DE2_115_Media_Computer/Software/Real_Time_Clock/Real_Time_Clock.c
@@ -0,0 +1,158 @@
+/*
+ * Real_Time_Clock.c
+ *
+ *  Created on: 10 de mar. de 2022
+ *      Author: Rocabado Rocha, Jose Luis
+ */
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Universitat Politècnica de València
+// Escuela Técnica Superior de Ingenieros de Telecomunicación
+// -------------------------------------------------------------------------------------------------------------------------
+// Sistemas Integrados Digitales
+// Curso 2021 - 2022
+// -------------------------------------------------------------------------------------------------------------------------
+// Nombre del archivo: Real_Time_Clock.c
+//
+// Descripción:
+// El siguiente codigo implementa la visualización de un reloj utilizando interruociones y el timer del sistema. Sus
+// especificaciones son:
+//      - Horas, minutos y segundos se observan en los displays 7-seg
+//      - El timer se configura para generar una interrupción cada 100ms con un reloj del sistema de 50MHz
+//      - Pulsador KEY3 incrementa las horas en una unidad
+//      - Pulsador KEY2 incrementa los minutos en una unidad
+//      - El reloj se visualiza ademas en el LCD y en la otra fila se mostrara el nombre
+//
+// -------------------------------------------------------------------------------------------------------------------------
+//      Versión: V1.0                   | Fecha Modificación: 10/03/2022
+//
+//      Autor: Jose Luis Rocabado Rocha
+//
+// -------------------------------------------------------------------------------------------------------------------------
+
+#include "key_codes.h"	// define los valores para KEY1, KEY2, KEY3
+#include "system.h"
+#include "sys/alt_irq.h"
+#include <stdio.h> //Necesario para el NULL
+
+// Definition of the auxiliar functions
+void interval_timer_isr( );
+void pushbutton_ISR( );
+int hex_to_seven (int hex);
+int two_hex_to_seven (int two_hex);
+
+volatile int Update_time_flag = 0;
+
+int main(void)
+{
+	///*Variable definition*///
+	// 6 bits para 60, 5 bits para 24. 11
+	int time = 0; //[5:0]s, [11:6]min, [16:12] hours.
+	int h_7seg, m_7seg, s_7seg;
+
+	/* Peripheral address definitions */
+	volatile int * interval_timer_ptr = (int *) TIMER_BASE;	    // Dirección Temporizador
+	volatile int * KEY_ptr = (int *) PUSHBUTTONS_BASE;			// Dirección pulsadores KEY
+	volatile int * HEX3_HEX0_ptr	= (int *) HEX3_HEX0_BASE;	// Dirección HEX3_HEX0
+	volatile int * HEX7_HEX4_ptr	= (int *) HEX7_HEX4_BASE;	// Dirección HEX7_HEX4
+
+	/* Configuring timer */
+	// 0x4C4B40 -> 5000000 counts
+	*(interval_timer_ptr + 0x2) = ( 0x4C4B40 & 0xFFFF); // LCounter
+	*(interval_timer_ptr + 0x3) = ( 0x4C4B40 >> 16) & 0xFFFF; // HCounter
+
+	/* Enabling timer and Interrupt Requests */
+	*(interval_timer_ptr + 1) = 0x7;	// STOP = 0, START = 1, CONT = 1, ITO = 1
+	//alt_irq_register(TIMER_IRQ, NULL, interval_timer_isr);
+
+	/* Configuring pushbuttons */
+	*(KEY_ptr + 2) = 0xC; 		/* Enabling interrupts for KEY2 y KEY3*/
+	*(KEY_ptr + 3) = 0; /* Edge detect reset */
+	//alt_irq_register(PUSHBUTTONS_IRQ, NULL, pushbutton_ISR);
+
+	/* Initialazing 7-segment and LCD*/
+
+	// Hour mask -> 0x1F000 >> 12
+	// Minute mask -> 0xFC0 >> 6
+	// Second mask -> 0x3F
+
+	// test C1E1E
+	time = 0xC1E1E;
+	while(1)
+	{
+		h_7seg = two_hex_to_seven(time >> 12 & 0xFF);
+		m_7seg = two_hex_to_seven(time >> 6 & 0xFF);
+		s_7seg = two_hex_to_seven(time & 0xFF);
+		printf("%x, ", time);
+		printf("%x, ", time >> 12 & 0xFF);
+		printf("%x, ", time >> 6 & 0xFF);
+		printf("%x",time & 0xFF);
+		return;
+		*(HEX3_HEX0_ptr) = s_7seg << 14;				// Visualiza el patrón en HEX3 ... HEX0
+	    *(HEX7_HEX4_ptr) = h_7seg << 7 | m_7seg;
+		if(Update_time_flag)
+		{
+			time++;
+		}
+	}
+
+}
+
+/****************************************************************************************
+ * Hexadecimal to 7-segment decoder
+****************************************************************************************/
+int hex_to_seven (int hex)
+{
+	switch(hex)
+	{
+		case 0x0: return 0b0111111;
+				  break;
+		case 0x1: return 0b0000110;
+				  break;
+		case 0x2: return 0b1011011;
+		  	  	  break;
+		case 0x3: return 0b1001111;
+		  	  	  break;
+		case 0x4: return 0b1100110;
+		  	  	  break;
+		case 0x5: return 0b1101101;
+		  	  	  break;
+		case 0x6: return 0b1111101;
+		  	  	  break;
+		case 0x7: return 0b0100111;
+		  	  	  break;
+		case 0x8: return 0b1111111;
+		  	  	  break;
+		case 0x9: return 0b1100111;
+		  	  	  break;
+		case 0xa: return 0b1110111;
+		  	  	  break;
+		case 0xb: return 0b1111100;
+		  	  	  break;
+		case 0xc: return 0b0111011;
+		  	  	  break;
+		case 0xd: return 0b1011110;
+		  	  	  break;
+		case 0xe: return 0b1111001;
+		  	  	  break;
+		case 0xf: return 0b1110001;
+		  	  	  break;
+		default: return 0b1000000;
+		  	  	  break;
+	}
+}
+
+/****************************************************************************************
+ * two digit hex to seven-segment
+****************************************************************************************/
+int two_hex_to_seven (int two_hex)
+{
+	int Lresult = hex_to_seven(two_hex & 0xF);
+	int Hresult = hex_to_seven(two_hex >> 4);
+	return (Hresult <<7) + Lresult;
+}
+
+
+
+
+
--- Ejericicio3/DE2_115_Media_Computer/db/ip/nios_system/submodules/nios_system_sysid.v
+++ Ejericicio3/DE2_115_Media_Computer/db/ip/nios_system/submodules/nios_system_sysid.v
@@ -49,7 +49,7 @@ module nios_system_sysid (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1646670123 : 0;
+  assign readdata = address ? 1646932264 : 0;
 
 endmodule
 
