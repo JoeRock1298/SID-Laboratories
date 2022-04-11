928486aaebb4ae9d045d25985145f66ed5cf8407
Jose Luis Rocabado Rocha <JOROROC@alumno.upv.es>
2022-04-11 10:38:36 +0200

    commit con los programas archivados, falta realizar el método 3 y la memoria

--- Tarea3/aceleracion_partida/db/ip/sld71843a6b/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ Tarea3/aceleracion_partida/db/ip/sld71843a6b/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 425e8896dea6b8a4e72b
+//   DESIGN_HASH   : a6db64e700f9fb335128
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h425e8896dea6b8a4e72b };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha6db64e700f9fb335128 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
