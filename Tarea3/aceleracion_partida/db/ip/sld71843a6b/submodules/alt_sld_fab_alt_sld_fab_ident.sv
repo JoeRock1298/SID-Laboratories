347f67e21f5c7166ed85c4c1ba2252f58e6e7f0d
Jose Luis Rocabado Rocha <JOROROC@alumno.upv.es>
2022-05-02 11:00:53 +0200

    commit con archivos de la entrega

--- Tarea3/aceleracion_partida/db/ip/sld71843a6b/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ Tarea3/aceleracion_partida/db/ip/sld71843a6b/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : e56e2c64adeb98a24f0d
+//   DESIGN_HASH   : a6db64e700f9fb335128
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'he56e2c64adeb98a24f0d };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha6db64e700f9fb335128 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
